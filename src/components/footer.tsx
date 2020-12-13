4c581872fce03f748d9b0de83d1adeab72630ed0
loxygenK <loxygen.k@gmail.com>
2020-12-14 00:35:30 +0900

    💬 Changed footer to the link to source codes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,19 +3,13 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
-      <span>Deploy your own!</span>
-      <ExtLink href="https://vercel.com/import/git?s=https://github.com/ijjk/notion-blog/tree/master&env=NOTION_TOKEN,BLOG_INDEX_ID&envDescription=Required+env+values+for+deploying&envLink=https://github.com/ijjk/notion-blog%23getting-blog-index-and-token">
-        <img
-          src="https://vercel.com/button"
-          height={46}
-          width={132}
-          alt="deploy to Vercel button"
-        />
-      </ExtLink>
       <span>
-        or{' '}
+        <ExtLink href="https://github.com/loxygenk/notion-blog">
+          view derived source
+        </ExtLink>
+        {' - '}
         <ExtLink href="https://github.com/ijjk/notion-blog">
-          view source
+          view original source
         </ExtLink>
       </span>
     </footer>
