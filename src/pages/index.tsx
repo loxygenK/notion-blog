385d9d0faa26095c1fc670e16e35ef265dfd73b0
loxygenK <loxygen.k@gmail.com>
2020-12-13 23:12:52 +0900

    🚚 Moved outlineCode class to proper location

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,15 @@
-import Link from 'next/link'
 import Header from '../components/header'
-import ExtLink from '../components/ext-link'
-import Features from '../components/features'
-import GitHub from '../components/svgs/github'
 import sharedStyles from '../styles/shared.module.css'
-import homeStyle from '../styles/home.module.css'
 
 export default () => (
   <>
     <Header titlePre="Home" />
     <div className={sharedStyles.layout}>
-      <div className={homeStyle.outlineCode}>
+      <div className="outlineCode">
         <h1>
           [visitor@flisan ~]
-          <span className={homeStyle.userTypeChar}>$ </span>
-          <span className={homeStyle.cursor} />
+          <span className="userTypeChar">$ </span>
+          <span className="cursor" />
         </h1>
       </div>
       <h2>Inspect what Flisan look like</h2>
